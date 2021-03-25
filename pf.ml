8c8b193b675ca5da6ce28ed6574610807e8b141d
PIERRE Guillaume <proguillaume.pierre@gmail.com>
2021-03-25 12:10:28 +0100

    push

--- pf.ml
+++ pf.ml
@@ -18,17 +18,24 @@ t (List.hd f);;
 let aridity t =
   match t with
   | Add -> 2
-  | Subtract -> 2   
-  | Multiply -> 2  
-  | Divide -> 2   
-  | Minus -> 2   
+  | Subtract -> 2
+  | Multiply -> 2
+  | Divide -> 2
+  | Minus -> 2
   | Variable t-> 0
   | Number t-> 0
-  | End -> 1   
+  | End -> 1
 ;;
 
+let a = Binary(Plus, Cst(2), Cst(2));;
 
 
-let parse exp = 
-  
-  ;;
+let parse exp =
+  let rec parse_aux exp_aux arb = 
+  match exp_aux with
+  |[] -> failwith"Expression Vide !"
+  |h::t -> if aridity h = 2
+            then match abr with
+            | Unary -> Binary(h, Unary, Unary)
+            | Binary(v, l, r) -> Binary(h, Binary(v, l, r), Unary )
+  ;; 
