f2e46ea7cad989808c749383564736e0a03ec9d9
HizeOok <47179772+HizeOok@users.noreply.github.com>
2021-04-08 21:22:05 +0200

    rien

--- pf.ml
+++ pf.ml
@@ -61,41 +61,34 @@ let parse exp =
 
   Format.printf "test %s" a;;
   
-  let change_op_in_expression v =
+let change_op_in_expression v =
     match v with
     | Plus ->  '+';
     | Minus ->'-';
     | Mult -> '*';
     | Div -> '/';
-    ;;
-    (change_op_in_expression Plus);;
-    let change_tree_in_expression v =
-      match v with
-
-      | Cst(t)-> t;
-      | Var(t)->  t;
-      ;;
-
-
+;;
 
   
-    let rec abr_to_list_bis( tree, lst) =
-     match tree with 
-     | Binary(v, l, r) -> match l with 
-                        | Cst(c) -> abr_to_list_bis( r, char_of_int(c)::lst )
-                        | Var(c) -> abr_to_list_bis( r, c::lst )
-                        | Binary(s, q, d) -> abr_to_list_bis( q, change_op_in_expression s::lst )
+let rec abr_to_list_bis( tree, lst) =
+  match tree with 
+  | Binary(v, l, r) -> match l with 
+                      | Cst(c) -> abr_to_list_bis( r, char_of_int(c)::lst )
+                      | Var(c) -> abr_to_list_bis( r, c::lst )
+                      | Binary(s, q, d) -> abr_to_list_bis( q, change_op_in_expression s::lst )
                         
-    | Binary(v, l, r) -> match r with
-                        | Cst(c) -> abr_to_list_bis( l, char_of_int(c)::lst )
-                        | Var(c) -> abr_to_list_bis( l, c::lst )
-                        | Binary(s, q, d) -> abr_to_list_bis( d, change_op_in_expression s::lst )
-  ;;
-  let abr_to_list tree =                   
+  | Binary(v, l, r) -> match r with
+                      | Cst(c) -> abr_to_list_bis( l, char_of_int(c)::lst )
+                      | Var(c) -> abr_to_list_bis( l, c::lst )
+                      | Binary(s, q, d) -> abr_to_list_bis( d, change_op_in_expression s::lst )
+;;
+
+
+let abr_to_list tree =                   
   abr_to_list_bis(tree, [])
   
-    ;;
+;;
 
-    let tree = Binary(Plus, Cst(5), Cst(5));;
-    #trace abr_to_list;;
-    abr_to_list tree;;
+let tree = Binary(Plus, Cst(5), Cst(5));;
+#trace abr_to_list;;
+abr_to_list tree;;
